5d009885c4e2c342427bb65e058f1243655c8f99
Rybnikov.VV <va6d6@mail.ru>
2022-03-03 02:26:34 +0500

    добавил функционал - изменние количества товара влияет на конечную сумму

--- src/plugins/list.js
+++ src/plugins/list.js
@@ -8,11 +8,20 @@ export class List {
         this._createList(lists)
         document.getElementById(options.id).append(this.listHTML)
 
-        this.listHTML.addEventListener('click', (event)=>{
-            const actionDeleteItem = event.target.dataset.listDeleteItem
+        this.listHTML.addEventListener('click', (event) => {
+            const btnDeleteItem = event.target.dataset.listDeleteItem
 
-            if (actionDeleteItem) {
-                this._deleteItem(event.target)
+            if (btnDeleteItem) {
+                this.deleteItem(event.target)
+                this._calcSum(this.data)
+            }
+        })
+
+        this.listHTML.addEventListener('input', (e) => {
+            const changeCount = e.target.dataset.listChangeCount
+
+            if (changeCount) {
+                this.cnageCountValue(e.target)
                 this._calcSum(this.data)
             }
         })
@@ -31,21 +40,21 @@ export class List {
             this.sumPrice += listItem.count * listItem.price
 
             this.listHTML.insertAdjacentHTML('beforeend', 
-            `<div class="list__item">
-                <div class="d-flex aligin-center list__item-content">
-                    <div class="list__img">
-                        <img src="./img/${listItem.img}" alt="">
+                `<div class="list__item">
+                    <div class="d-flex aligin-center list__item-content">
+                        <div class="list__img">
+                            <img src="./img/${listItem.img}" alt="">
+                        </div>
+                        <div class="list__text">${listItem.title}</div>
+                        <div>Цена:${listItem.price}</div>
+                        <div>Кол-во:<input type="number" value="${listItem.count}" data-list-change-count="${listItem.id}">кг</div>
+                        <div class="list__price">Итоговая цена:${listItem.count * listItem.price}</div>
                     </div>
-                    <div class="list__text">${listItem.title}</div>
-                    <div>Цена:${listItem.price}</div>
-                    <div>Кол-во:<input type="number" value="${listItem.count}" data-list="changeCount">кг</div>
-                    <div class="list__price">Итоговая цена:${listItem.count * listItem.price}</div>
-                </div>
-                <div class="list__item-actions">
-                    <div class="btn_info" data-modalInfo="${listItem.id}">Подробнее</div>
-                    <div class="btn_danger" data-list-delete-item="${listItem.id}">Удалить</div>
-                </div>
-            </div>`)
+                    <div class="list__item-actions">
+                        <div class="btn_info" data-modalInfo="${listItem.id}">Подробнее</div>
+                        <div class="btn_danger" data-list-delete-item="${listItem.id}">Удалить</div>
+                    </div>
+                </div>`)
           })
 
 
@@ -62,12 +71,26 @@ export class List {
         })
     }
 
-    _deleteItem(elem) {
+    deleteItem(elem) {
         this.data = this.data.filter(item => {
             return item.id !== elem.dataset.listDeleteItem
         })
 
         elem.closest('.list__item').classList.add('hidden')
+    }
+
+    cnageCountValue(elem) {
+        const elemId = elem.dataset.listChangeCount
+        const newCountValue = elem.closest('.list__item').querySelector('input').value
+        const targetItemPrice = elem.closest('.list__item').querySelector('.list__price')
+        
+        this.data.find((item) => {
+            if (item.id === elemId) {
+                item.count = newCountValue
+                targetItemPrice.innerHTML = item.count * item.price
+                return true
+            } 
+        })
     }
 
     _calcSum(data) {
@@ -80,3 +103,5 @@ export class List {
         this.listHTML.querySelector('[data-list-sum-price]').innerHTML = `ИТОГ:<b>${this.sumPrice}</b>`
     }
 }
+
+// 1. при загрузке 25.02.22 1:44 данные запрашиваются за период 24.02.22-25.02.22 но в дискретности показывается 25.02.22
